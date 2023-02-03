541db533d5da65749c7a7c5acc423e24509d2db8
Jeffrey Zhang <96641963+cleansR@users.noreply.github.com>
2023-02-03 09:30:37 -0500

    Update cf.cpp

--- cf.cpp
+++ cf.cpp
@@ -21,13 +21,15 @@ typedef long double ld;
 #define MOD 1000000007
 #define otherMOD 998244353
 
-namespace sg {
-    typedef ll T;
+// Segtree
+struct segtree{
+    typedef ll T; //replace this
+
     T id = 0, t[2 * N]; 
     // id is identity: 1 for mult, 0 for add, ll max for min, ll min for max
     // Make sure N is >= siz of array
     
-    T func(T a, T b) { return a+b; }
+    T func(T a, T b) { return a+b; } // replace this
  
     void modify(ll p, T v) { //set value v at position p
         for(t[p += N] = v; p /= 2;) t[p] = func(t[2 * p], t[2 * p + 1]);
@@ -41,75 +43,43 @@ namespace sg {
         }
         return func(resl, resr);
     }
- 
-    void print(ll n){
- 
-    }
-}
- 
-void dfs(map<ll, vector<ll>>& mp, ll curr, vector<ll>& visited)
-{
-    visited[curr] = true;
-    for(auto it: mp[curr]){
-        if(!visited[it]){
-          dfs(mp, it, visited);
+};
+
+class segtree{
+    public:
+        typedef ll T; //replace this
+
+        T id; vector<T> t;
+
+        segtree(){
+            id = 0; //replace this
+            t = vector<T>(2*N, 0);
         }
-    }
-}
+
+        segtree(ll siz){
+            id = 0; //replace this
+            t = vector<T>(2*siz, 0);
+        }
+
+        // id is identity: 1 for mult, 0 for add, ll max for min, ll min for max
+        // Make sure N is >= siz of array
+        
+        T func(T a, T b) { return a+b; } // replace this
+    
+        void modify(ll p, T v) { //set value v at position p
+            for(t[p += N] = v; p /= 2;) t[p] = func(t[2 * p], t[2 * p + 1]);
+        }
+    
+        T query(ll l, ll r) { //query on interval [l, r)
+            T resl = id, resr = id;
+            for(l += N, r += N; l < r; l /= 2, r /= 2) {
+                if(l & 1) resl = func(resl, t[l++]);
+                if(r & 1) resr = func(t[--r], resr);
+            }
+            return func(resl, resr);
+        }
+};
  
-bool isPrime(ll n)
-{
-  for(ll i = 2; i<= (ll)sqrt(n); i++)
-  {
-    if(n%i==0)
-    {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool isPalindrome(string s)
-{
-  ll l = 0; ll r = s.length(); r--;
-  while(l<r)
-  {
-    if(s[l]==s[r])
-    {
-      l++; r--;
-    }
-    else return false;
-  }
-  return true;
-}
-
-bool works()
-{
- return false;
-}
-
-vector<ll> prefixSumA(ll arr[], ll size)
-{
-  vector<ll> pF;
-  pF.push_back(0);
-  for(ll i = 1; i <=size; i++)
-  {
-    pF.push_back(pF[i-1]+arr[i-1]);
-  }
-  return pF;
-}
- 
-vector<ll> prefixSumV(vector<ll>& v)
-{
-  vector<ll> pF;
-  pF.push_back(0);
-  for(ll i = 1; i <=v.size(); i++)
-  {
-    pF.push_back(pF[i-1]+v[i-1]);
-  }
-  return pF;
-}
-
 void treeDFS(vector<ll> adj[], ll curr, ll parent)
 {
   vector<ll> temp = adj[curr];
@@ -120,14 +90,22 @@ void treeDFS(vector<ll> adj[], ll curr, ll parent)
   }
 }
 
-bool sortbyCond(const pair<pair<ll,ll>, vector<ll>>& a, const pair<pair<ll,ll>, vector<ll>>& b)
+void dfs(map<ll, vector<ll>>& mp, ll curr, vector<ll>& visited)
 {
-    if (a.first.first != b.first.first)
-        return (a.first < b.first);
-    else
-        return (a.first.second > b.first.second);     
+    visited[curr] = true;
+    for(auto it: mp[curr]){
+        if(!visited[it]){
+          dfs(mp, it, visited);
+        }
+    }
 }
  
+//Bsearch
+bool works()
+{
+    return false;
+}
+
 ll bsBG(ll l, ll r) //Binary Search [Bad, Good]
 {
   while(r-l>1)
@@ -161,7 +139,20 @@ ll bsGB(ll l, ll r) //Binary Search [Good, Bad]
   }
   return l;
 }
+
+
+bool sortbyCond(const pair<pair<ll,ll>, vector<ll>>& a, const pair<pair<ll,ll>, vector<ll>>& b)
+{
+    if (a.first.first != b.first.first)
+        return (a.first < b.first);
+    else
+        return (a.first.second > b.first.second);     
+}
  
+
+
+//Combinatorics
+
 ll inv(ll a, ll b){return 1<a?b - inv(b%a,a)*b/a : 1;}
  
 vector<ll>fact(ll n) // MOD MOD
@@ -176,7 +167,23 @@ ll NChooseK(vector<ll>& v, ll n, ll k) //MOD
   if(k>0) return 0;
   else return v[n] * inv(v[n-k], MOD) % MOD * inv(v[k], MOD) % MOD;
 }
- 
+
+
+//Number Theory
+
+ll binpow(ll a, ll b) 
+{ 
+	ll res = 1;
+	while (b > 0) {
+		if (b & 1) res *= a;
+		a *= a;
+		res %= MOD;
+		a %= MOD;
+		b >>= 1;
+	}
+	return res;
+}
+
 ll gcd(ll a, ll b)
 { if(b==0)return a; else return gcd(b,a%b); }
  
